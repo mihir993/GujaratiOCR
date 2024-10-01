ab06de39304d636a568e6e830a0dfc16ac4452c2
MShah <mihir.993@gmail.com>
2024-10-01 21:45:50 +0200

    Model training and testing works.

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 from model.KerasCNN import KerasCNN
 from utils.GujaratiOCRDataset import GujaratiOCRDataset
 import numpy as np
+from keras import datasets, ops
+from sklearn.preprocessing import OneHotEncoder
 
 
 # Press the green button in the gutter to run the script.
 def get_model(image_sample, num_categories):
-    return KerasCNN(input_image_size=image_sample.shape[0:2],
-                    input_channels=image_sample.shape[2],
+    return KerasCNN(input_image_sample=image_sample,
                     num_conv_layers=2,
                     output_categories=num_categories)
 
@@ -18,13 +19,21 @@ def get_data_set():
 
 if __name__ == '__main__':
     data_set = get_data_set()
+    train_labels = data_set.training.labels
+    onehot = OneHotEncoder()
+    onehot.fit(train_labels.reshape(-1, 1))
+
     print(f"dataset: {data_set.training.images.shape}")
     print(f"dataset: {data_set.testing.images.shape}")
     cnn_model = get_model(data_set.training.images[0], np.unique(data_set.training.labels).size)
+    cnn_model.label_encoder = onehot
     print(cnn_model.model.summary())
-    # cnn_model.train(data_set.training)
-    # print("------------> training finished")
-    # cnn_model.test(data_set.test)
-    # print("------------> testing finished")
-    # print("")
+    cnn_model.train(data_set.training)
+    print("------------> training finished")
+    cnn_model.test(data_set.testing)
+    print("------------> testing finished")
+    # print(cnn_model.evaluate)
+    cnn_model.model.save("./model.keras")
+    cnn_model.model.save_weights("./model.weights.h5")
+    print("finished saving")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- model/KerasCNN.py
+++ model/KerasCNN.py
@@ -1,17 +1,20 @@
 from keras import models, layers, losses
+from keras import utils as kutils
 
+kutils.set_random_seed(812)
 
 class KerasCNN:
-    def __init__(self, input_image_size, input_channels, num_conv_layers, output_categories):
+    def __init__(self, input_image_sample, num_conv_layers, output_categories):
+        self.label_encoder =None
         self.model = models.Sequential()
-        self.add_input_convolution_layer(16, (input_image_size[0], input_image_size[1], input_channels))
+        self.add_input_convolution_layer(16, input_image_sample.shape)
         for i in range(num_conv_layers - 1):
             self.add_convolution_layer(8)
         self.model.add(layers.Flatten())
         self.model.add(layers.Dense(8, input_dim=4, activation='relu'))
         self.model.add(layers.Dense(output_categories, activation='softmax'))
         self.model.compile(optimizer='adam',
-                           loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+                           loss=losses.categorical_crossentropy,
                            metrics=['accuracy'])
 
     def add_input_convolution_layer(self, filters: int, input_shape: tuple([int, int, int])):
@@ -30,11 +33,17 @@ class KerasCNN:
         self.model.add(layers.MaxPool2D(2, 2))
 
     def train(self, training_set):
-        history = self.model.fit(training_set.images, training_set.labels, epochs=10,
-                       validation_data=(training_set.images, training_set.labels))
+        history = self.model.fit(training_set.images,
+                                 self.encode_labels(training_set),
+                                 epochs=10,  verbose=True,
+                                 validation_data=(training_set.images, self.encode_labels(training_set))
+                                 )
         print(history)
 
-
     def test(self, testing_set):
-        eval = self.model.evaluate(testing_set.images, testing_set.labels)
+        eval = self.model.evaluate(testing_set.images,
+                                   self.encode_labels(testing_set))
         print(eval)
+
+    def encode_labels(self, data):
+        return self.label_encoder.transform(data.labels.reshape(-1, 1)).toarray()
